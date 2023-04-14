bb4d74a9e61764b1a5066afd08ac9d960e45ca83
Zafer Çetin <zaferkurumsal@gmail.com>
2023-04-14 11:20:05 +0300

    fast

--- lib/video_screen.dart
+++ lib/video_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:flutter_reels/video_controller.dart';
