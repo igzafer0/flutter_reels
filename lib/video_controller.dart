ca89d6c8cad0ec3de109b384bfa2e79c885ee14f
Zafer Çetin <zaferkurumsal@gmail.com>
2023-04-14 09:51:38 +0300

    fast

--- lib/video_controller.dart
+++ lib/video_controller.dart
@@ -7,28 +7,18 @@ import 'package:video_player/video_player.dart';
 
 class VideoController extends StatefulWidget {
   late VideoPlayerController controller;
-
-  VideoController({super.key});
+  String url;
+  VideoController({super.key, required this.url});
 
   @override
   State<VideoController> createState() => _VideoControllerState();
 }
 
 class _VideoControllerState extends State<VideoController> {
-  var videoList = [
-    "https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-fashion-woman-with-silver-makeup-39875-large.mp4",
-    "https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4",
-    "https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4",
-    "https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4",
-    "https://mixkit.co/free-stock-video/black-and-orange-tarantula-walking-vertical-shot-1482/",
-    "https://assets.mixkit.co/videos/preview/mixkit-a-surfer-walking-on-the-beach-with-a-surfboard-1220-large.mp4",
-    "https://assets.mixkit.co/videos/preview/mixkit-urban-man-puts-on-a-glasses-at-a-dark-room-1235-large.mp4"
-  ];
   @override
   void initState() {
     super.initState();
-    widget.controller = VideoPlayerController.network(
-        videoList[Random().nextInt(videoList.length)])
+    widget.controller = VideoPlayerController.network(widget.url)
       ..initialize().then((_) {
         setState(() {
           widget.controller.setLooping(true);
--- lib/video_screen.dart
+++ lib/video_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_reels/video_controller.dart';
 
@@ -17,6 +19,15 @@ class _VideoScreenState extends State<VideoScreen> {
   }
 
   List<VideoController> dataList = [];
+  var videoList = [
+    "https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-fashion-woman-with-silver-makeup-39875-large.mp4",
+    "https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4",
+    "https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4",
+    "https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4",
+    "https://mixkit.co/free-stock-video/black-and-orange-tarantula-walking-vertical-shot-1482/",
+    "https://assets.mixkit.co/videos/preview/mixkit-a-surfer-walking-on-the-beach-with-a-surfboard-1220-large.mp4",
+    "https://assets.mixkit.co/videos/preview/mixkit-urban-man-puts-on-a-glasses-at-a-dark-room-1235-large.mp4"
+  ];
   int oldValue = 0;
   @override
   Widget build(BuildContext context) {
@@ -31,7 +42,8 @@ class _VideoScreenState extends State<VideoScreen> {
                 oldValue = value;
               },
               itemBuilder: ((context, index) {
-                dataList.add(VideoController());
+                dataList.add(VideoController(
+                    url: videoList[Random().nextInt(videoList.length)]));
                 return dataList[index];
               }))),
     );
